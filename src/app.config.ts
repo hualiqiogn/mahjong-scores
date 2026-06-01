8ac1114e3ee9f93d9a9b2dc21852fdafd841d262
huali <2055440367@qq.com>
2026-06-01 23:32:56 +0800

    feat: 添加按需注入配置 lazyCodeLoading: requiredComponents

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,6 @@
 export default defineAppConfig({
   pages: ['pages/index/index'],
+  lazyCodeLoading: 'requiredComponents',
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#ffffff',
